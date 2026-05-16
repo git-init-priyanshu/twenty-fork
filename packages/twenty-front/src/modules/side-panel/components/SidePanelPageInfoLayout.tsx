1bf5fbc414655553b3c104869124e8e5205660c1
Priyanshu Bartwal <bartwalpriyanshu00@gmail.com>
2026-05-17 01:01:26 +0530

    Fix: Title color of side panel with multi records info not following the theme correctly

--- packages/twenty-front/src/modules/side-panel/components/SidePanelPageInfoLayout.tsx
+++ packages/twenty-front/src/modules/side-panel/components/SidePanelPageInfoLayout.tsx
@@ -29,6 +29,7 @@ export const StyledPageInfoTextContainer = styled.div`
 `;
 
 export const StyledPageInfoTitleContainer = styled.div`
+  color: ${themeCssVariables.font.color.tertiary};
   font-size: ${themeCssVariables.font.size.md};
   font-weight: ${themeCssVariables.font.weight.semiBold};
   max-width: 150px;
